986937225abaf85f755b2149f46d76fdc8109847
Hernán Peñalbé <114519327+Elwilsonic@users.noreply.github.com>
2025-11-30 21:29:39 -0300

    Se borró la clave foranea especialidad

--- app/models/alumno.py
+++ app/models/alumno.py
@@ -17,7 +17,5 @@ class Alumno(db.Model):
     fecha_nacimiento:date = db.Column(db.Date, nullable=False)
     sexo:str = db.Column(db.String(1), nullable=False) 
     nro_legajo:int = db.Column(db.Integer, nullable=False)
-    fecha_ingreso:date = db.Column(db.Date, nullable=False) 
-
-    especialidad_id:int = db.Column(db.Integer, db.ForeignKey('especialidades.id'), nullable=False)
+    fecha_ingreso:date = db.Column(db.Date, nullable=False)
     
